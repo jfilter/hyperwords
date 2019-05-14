027ae3f3de37aa2d20cd3581a37bace00c14ef0c
Johannes Filter <hi@jfilter.de>
2019-05-14 17:14:42 +0200

    improve project setup

--- scripts/install_word2vecf.sh
+++ scripts/install_word2vecf.sh
@@ -8,3 +8,4 @@ mv yoavgo-word2vecf-1b94252a58d4/*.h word2vecf/.
 mv yoavgo-word2vecf-1b94252a58d4/makefile word2vecf/.
 rm -r yoavgo-word2vecf-1b94252a58d4
 make -C word2vecf
+
--- scripts/pairs2counts.sh
+++ scripts/pairs2counts.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sort -T . $1 | uniq -c
+
